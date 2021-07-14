c2b72fb8ec0673f225bbc1b713420dcef34c2f92
atef <mail@aamri.de>
2021-07-14 22:41:10 +0200

    generate Todo-List & checkbox for marked as done

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-let inputStr = "";
 const tasks = [];
 let myTasks = document.querySelector("#myTasks");
 
-const btn = document.querySelector("button");
+const btn = document.querySelector("#add");
 console.log(btn);
 
 function addTask() {
-	inputStr = document.getElementById("input").value;
+	const inputStr = document.getElementById("input").value;
 	console.log(inputStr);
+	tasks.push(inputStr);
+	console.log(tasks);
+	const newLi = document.createElement("li");
+	myTasks.appendChild(newLi);
+	newLi.innerText = inputStr;
+	// 	<input type="checkbox" />
+	const checkbox = document.createElement("input");
+	checkbox.type = "checkbox";
+	newLi.appendChild(checkbox);
 }
-
-addTask();
+btn.addEventListener("click", addTask);
